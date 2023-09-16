d61437054a6b1fff356b18653e79829e0c3d1f02
rodrigo <rodrigo.azvd750@gmail.com>
2023-09-15 22:09:22 -0300

    update pages

--- newsletter/src/app/page.tsx
+++ newsletter/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col gap-10 flex-1 items-center justify-center px-48">
       <section className="text-white text-5xl text-center">
-        Explore o Universo do Front-End: Receba as últimas tendências, dicas e truques toda semana!      </section>
+        Explore o Universo do Front-End: Receba as últimas tendências, dicas e truques toda semana!</section>
       <section className="text-white text-lg text-center">
         Inscreva-se na nossa newsletter semanal para ficar atualizado sobre as últimas novidades em desenvolvimento front-end, descobrir novas ferramentas, aprimorar suas habilidades e aprofundar seu conhecimento nesta área dinâmica!
       </section>
