3ad6354e480f8ede6db37a0ec0a6aa9cb63c9e25
rodrigo azevedo silva <rodrigo.azvd750@gmail.com>
2023-09-18 18:52:00 -0300

    update apiRoutes

--- newsletter-nextjs/src/db.ts
+++ newsletter-nextjs/src/db.ts
@@ -1,10 +1,12 @@
+import { createPool } from 'mysql2';
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
   host: 'containers-us-west-43.railway.app',
   user: 'root',
   password: 'E7n7QCwamdzYwR8wXRUU',
-  database: 'newsletter-nextjs',
+  port: 6607,
+  database: 'railway',
 });
 
 const connection = pool;
