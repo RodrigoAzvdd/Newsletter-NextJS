3bb5e8ea5c28d5a56a3056474939d5e1fbcd4dac
rodrigo azevedo silva <rodrigo.azvd750@gmail.com>
2024-02-15 14:52:25 -0300

    update sublist getSubs

--- newsletter-nextjs/src/components/SubList.tsx
+++ newsletter-nextjs/src/components/SubList.tsx
@@ -4,8 +4,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 interface Subscribers {
-  create_time: Date;
-  update_time: Date;
+  createdAt: Date;
+  updatedAt: Date;
   name: string;
   email: string;
   id: number;
@@ -16,9 +16,10 @@ const SubList = () => {
 
   const getSubs = async () => {
     try {
-        const response = await axios.get("https://newsletter-next-js.vercel.app/api/subscribers");
-        const data = response.data;
-      setSubscribers(data);
+      const response = await axios.get("https://test-db-prod.vercel.app/api/users");
+      const data = response.data;
+      console.log(data);
+      setSubscribers(data.users);
     } catch (error) {
       console.error("Error fetching subscribers:", error);
     }
@@ -30,7 +31,32 @@ const SubList = () => {
 
   return (
     <>
-      {JSON.stringify(subscribers)}
+      <table className="min-w-full">
+        <thead>
+          <tr className="text-left">
+            <th className="font-semibold px-5 border-b-2 border-gray-500 text-white text-lg p-2 w-1/4">ID</th>
+            <th className="font-semibold px-5 border-b-2 border-gray-500 text-white text-lg p-2 w-1/2">NOME</th>
+            <th className="font-semibold px-5 border-b-2 border-gray-500 text-white text-lg p-2 w-1/4">CRIADO EM</th>
+          </tr>
+        </thead>
+        <tbody>
+          {subscribers.length === 0 ?
+            <tr className="text-center">
+              <td className="text-center pt-10" colSpan={3}>Nenhum Assinante...</td>
+            </tr>
+            :
+            subscribers.map((sub: Subscribers) => (
+              <tr key={sub.id} className="h-10">
+                <td className="px-5 text-lg border-none">{sub.id}</td>
+                <td className="px-5 text-lg border-none">{sub.name}</td>
+                <td className="px-5 text-lg border-none">
+                  {new Date(sub.createdAt).toLocaleDateString()} as {new Date(sub.createdAt).toLocaleTimeString()}
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </>
   );
 };
