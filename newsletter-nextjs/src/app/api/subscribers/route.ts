a2af9e68983279371400a55ff7f83466ee8c0b9c
rodrigo azevedo silva <rodrigo.azvd750@gmail.com>
2024-02-15 14:00:38 -0300

    update api reqs

--- newsletter-nextjs/src/app/api/subscribers/route.ts
+++ newsletter-nextjs/src/app/api/subscribers/route.ts
@@ -1,24 +1,11 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import axios, { AxiosError } from 'axios';
+import { NextResponse } from 'next/server';
 
-interface ErrorResponse {
-    error: string;
-}
-
-export default async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(request: Request) {
     try {
         const response = await axios.get("https://test-db-prod.vercel.app/api/users");
-        res.status(200).json(response.data);
+        NextResponse.json(response)
     } catch (error: any) {
-        if (axios.isAxiosError(error)) {
-            const axiosError = error as AxiosError<ErrorResponse>;
-            const status = axiosError.response?.status || 500;
-            const errorMessage = axiosError.response?.data.error || "Internal Server Error";
-            console.error("Error fetching data from API:", errorMessage);
-            res.status(status).json({ error: errorMessage });
-        } else {
-            console.error("Error fetching data from API:", error);
-            res.status(500).json({ error: "Internal Server Error" });
-        }
+        NextResponse.json(error)
     }
 }
